3b74a3be38f2ec4143cb92be171eb72712b372ff
Ebenezer Sefah <esefah@mit.edu>
2020-10-29 23:51:01 -0400

    adding app code

---
+++ README.md
@@ -0,0 +1,19 @@
+# URL Shortener
+A simple URL shortener made with Node, Express and Vue.js for Recitation 9 of Fall 2020 6.170 Software Studio.
+
+## Installation
+```
+npm install
+```
+
+## Usage
+
+```
+npm run build
+npm run serve
+
+npm run awesome //in a separate terminal tab
+```
+Navigate to `localhost:8080` in your browser and start creating shorts!
+
+Credit to [Harihar Subramanyam](https://github.com/hariharsubramanyam) for the idea.
---
+++ src/components/Navbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="nav-bar">
+    <div>
+      <router-link to="/account">
+        <span v-if="userName">
+          Hi {{userName}}!
+        </span>
+        <span v-else>
+          Sign In/Sign Up
+        </span>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "main",
+  data() {
+    return {
+      userName: this.$cookie.get('url-shortener-auth')
+    }
+  },
+};
+</script>
