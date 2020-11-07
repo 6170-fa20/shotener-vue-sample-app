125575b9bddef67bf83fc8a8d174dd3b668920ed
Ebenezer Sefah <esefah@mit.edu>
2020-11-07 17:19:04 -0500

    adding example of images

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,6 +2,9 @@
   <div class="nav-bar">
     <div>
       <router-link to="/account">
+        <div class="nav-icon-container">
+          <img src="/images/minimize_icon.png"/>
+        </div>
         <span v-if="userName">
           Hi {{userName}}!
         </span>
@@ -23,3 +26,16 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.nav-icon-container {
+    width: 20px;
+    height: 20px;
+    display: inline-block;
+}
+
+.nav-icon-container img {
+  width: inherit;
+  height: inherit;
+}
+</style>
